6e0eecf5cf1d1b45da5826eafeaf6fb73fdc566e
tamashiro-syuta <syuta5825@gmail.com>
2024-02-23 14:33:47 +0900

    ✨ vconsoleでログを端末絡みれるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import VConsole from "vconsole";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // NOTE: レンダリング時に初期化
+    const vConsole = new VConsole();
+
+    // NOTE: アンマウント時に解除
+    return () => {
+      vConsole.destroy();
+    };
+  }, []);
+
+  console.log("count: ", count);
 
   return (
     <>
@@ -29,7 +42,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
